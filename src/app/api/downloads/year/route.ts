bf7a3a54972e22d8c7eab8bbb200fe0a58a22d6e
tada-js <justfix010@gmail.com>
2024-08-12 00:58:13 +0900

    :sparkles: Feat : npm downloads api

---
+++ src/app/api/downloads/year/route.ts
@@ -0,0 +1,8 @@
+export async function GET(request: Request) {
+  const res = await fetch(
+    `https://api.npmjs.org/downloads/point/last-year/react-tada`
+  );
+
+  const data = await res.json();
+  return Response.json({ data });
+}
